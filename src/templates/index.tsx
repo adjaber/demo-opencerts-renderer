df2ec4be57a1fc5ab9230e5aee551f1b640fe506
adjebbar <45851643+adjebbar@users.noreply.github.com>
2021-07-28 10:21:21 +0200

    Update index.tsx

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -4,6 +4,7 @@ import { templates as certificateOfAwardTemplates } from "./certificate-of-award
 import { templates } from "./certificateOfAchievement";
 import { geekOut2020Template } from "./geekOut2020";
 import { geekOut2021Template } from "./geekOut2021";
+import { templates as drivingLicense } from "./driver-license-v3";
 import { TemplateRegistry } from "@govtechsg/decentralized-renderer-react-components";
 
 
@@ -15,5 +16,6 @@ export const registry: TemplateRegistry<any> = {
   GOVTECH_DEMO: govtechDemoTemplates,
   TESTIMONIALS: testimonialsTemplates,
   TESTIMONIALS2: testimonialsTemplates2,
-  CERTIFICATE_OF_AWARD: certificateOfAwardTemplates
+  CERTIFICATE_OF_AWARD: certificateOfAwardTemplates,
+  DRIVING_LICENSE: drivingLicense
 };

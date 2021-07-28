b6d30fec43d0904d506b3cbc4fc45e00ffc9d22b
adjebbar <45851643+adjebbar@users.noreply.github.com>
2021-07-28 09:52:32 +0200

    Update index.tsx

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,10 +1,17 @@
 import { templates as govtechDemoTemplates } from "./govtech-demo-cert";
 import { templates as testimonialsTemplates, templates2 as testimonialsTemplates2 } from "./testimonials";
 import { templates as certificateOfAwardTemplates } from "./certificate-of-award";
+import { templates } from "./certificateOfAchievement";
+import { geekOut2020Template } from "./geekOut2020";
+import { geekOut2021Template } from "./geekOut2021";
 import { TemplateRegistry } from "@govtechsg/decentralized-renderer-react-components";
+
 
 export const registry: TemplateRegistry<any> = {
   default: govtechDemoTemplates,
+  CERTIFICATE_OF_ACHIEVEMENT: templates,
+  GEEK_OUT_2020: geekOut2020Template,
+  GEEK_OUT_2021: geekOut2021Template,
   GOVTECH_DEMO: govtechDemoTemplates,
   TESTIMONIALS: testimonialsTemplates,
   TESTIMONIALS2: testimonialsTemplates2,
